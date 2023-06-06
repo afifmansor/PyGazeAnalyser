e1c12d4569517d563d4423f5c2a64135c3bdc6ad
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-06-06 15:54:46 +0800

    Update analysis.py

--- examples/analysis/analysis.py
+++ examples/analysis/analysis.py
@@ -157,21 +157,21 @@ for ppname in PPS:
 # Streamlit interface for uploading image
 uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
-                if uploaded_image is not None:
-                    # Save the uploaded image to a temporary directory
-                    image_path = os.path.join(DIR, "temp", uploaded_image.name)
-                    with open(image_path, "wb") as file:
-                        file.write(uploaded_image.getbuffer())
+if uploaded_image is not None:
+   # Save the uploaded image to a temporary directory
+   image_path = os.path.join(DIR, "temp", uploaded_image.name)
+   with open(image_path, "wb") as file:
+   file.write(uploaded_image.getbuffer())
 
-                    # Update the image file path
-                    imagefile = image_path
+   # Update the image file path
+   imagefile = image_path
 
-                    # Generate the heatmap using the uploaded image
-                    heatmapfile = os.path.join(pplotdir, "heatmap_%s_%d" % (ppname, trialnr))
-                    draw_heatmap(fixations, DISPSIZE, imagefile=imagefile, durationweight=True, alpha=0.5, savefilename=heatmapfile)
+   # Generate the heatmap using the uploaded image
+   heatmapfile = os.path.join(pplotdir, "heatmap_%s_%d" % (ppname, trialnr))
+   draw_heatmap(fixations, DISPSIZE, imagefile=imagefile, durationweight=True, alpha=0.5, savefilename=heatmapfile)
 
-                    # Display the uploaded image
-                    st.image(image_path, use_column_width=True)
+   # Display the uploaded image
+   st.image(image_path, use_column_width=True)
             
-                    # Display the resulting heatmap
-                    st.image(heatmapfile, use_column_width=True)
+   # Display the resulting heatmap
+   st.image(heatmapfile, use_column_width=True)
